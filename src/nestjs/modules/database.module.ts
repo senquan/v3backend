75689f044c60901b46a90f7c834dbc3e6cebc8fa
Senquan <senquan@sina.com>
2026-05-08 16:10:19 +0800

    fix: 修改TypeOrm实体导入方式

--- src/nestjs/modules/database.module.ts
+++ src/nestjs/modules/database.module.ts
@@ -1,51 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Bulletin } from '../../models/bulletin.model';
-import { Category } from '../../models/category.model';
-import { Coupon } from '../../models/coupon.model';
-import { CouponUser } from '../../models/coupon-user.model';
-import { Customer } from '../../models/customer.model';
-import { Dict } from '../../models/dict.model';
-import { ExpressCompany } from '../../models/express-company.model';
-import { ExpressTracking } from '../../models/express-tracking.model';
-import { Gallery } from '../../models/gallery.model';
-import { InviteCode } from '../../models/invite-code.model';
-import { LogArchive, LogChain, SystemLog } from '../../models/system-log.model';
-import { Notification } from '../../models/notification.model';
-import { Order } from '../../models/order.model';
-import { OrderCalculationLog } from '../../models/order-calculation-log.model';
-import { OrderItem } from '../../models/order-item.model';
-import { OrderStatusLog } from '../../models/order-status-log.model';
-import { Permission } from '../../models/permission.model';
-import { PlatformTags } from '../../models/platform-tags.model';
-import { Product } from '../../models/product.model';
-import { ProductModel } from '../../models/product-model.model';
-import { ProductSeries } from '../../models/product-series.model';
-import { ProductSeriesTag } from '../../models/product-series-tag.model';
-import { ProductTag } from '../../models/product-tag.model';
-import { ProductTbSku } from '../../models/product-tb-sku.model';
-import { Promotion } from '../../models/promotion.model';
-import { PromotionPlatforms } from '../../models/promotion-platforms.model';
-import { PromotionRule } from '../../models/promotion-rule.model';
-import { PromotionRuleV3 } from '../../models/promotion-rule-v3.model';
-import { PromotionV3 } from '../../models/promotion-v3.model';
-import { ReturnOrder } from '../../models/return-order.model';
-import { ReturnOrderItem } from '../../models/return-order-item.model';
-import { Role } from '../../models/role.model';
-import { RolePermission } from '../../models/role-permission.model';
-import { RolePlatforms } from '../../models/role-platforms.model';
-import { RoleTags } from '../../models/role-tags.model';
-import { Settings } from '../../models/settings.model';
-import { ShortLink } from '../../models/short-link.model';
-import { SpecGroup } from '../../models/spec-group.model';
-import { SpecItem } from '../../models/spec-item.model';
-import { Staff } from '../../models/staff.model';
-import { Tag } from '../../models/tag.model';
-import { Ticket } from '../../models/ticket.model';
-import { TicketAttachment } from '../../models/ticket-attachment.model';
-import { TicketComment } from '../../models/ticket-comment.model';
-import { User } from '../../models/user.model';
-import { UserRole } from '../../models/user-roles.model';
 
 @Module({
   imports: [
@@ -57,16 +11,7 @@ import { UserRole } from '../../models/user-roles.model';
       password: process.env.DB_PASS || 'password',
       database: process.env.DB_NAME || 'cardbug',
       entities: [
-        Bulletin, Category, Coupon, CouponUser, Customer, Dict,
-        ExpressCompany, ExpressTracking, Gallery, InviteCode, LogArchive,
-        LogChain, Notification, Order, OrderCalculationLog, OrderItem,
-        OrderStatusLog, Permission, PlatformTags, Product, ProductModel,
-        ProductSeries, ProductSeriesTag, ProductTag, ProductTbSku,
-        Promotion, PromotionPlatforms, PromotionRule, PromotionRuleV3,
-        PromotionV3, ReturnOrder, ReturnOrderItem, Role, RolePermission,
-        RolePlatforms, RoleTags, Settings, ShortLink, SpecGroup,
-        SpecItem, Staff, SystemLog, Tag, Ticket, TicketAttachment,
-        TicketComment, User, UserRole,
+        __dirname + '/../../models/*.model{.ts,.js}'
       ],
       synchronize: process.env.NODE_ENV !== 'production',
       logging: process.env.NODE_ENV !== 'production',
